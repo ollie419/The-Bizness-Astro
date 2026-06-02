258f5564d61add545b91d1bd71ba43ed708d64cd
Ollie <info@ollieadegboye.com>
2026-06-02 23:21:01 +0100

    update

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: 'rscqwpq1',
-  dataset: 'production',
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || 'rscqwpq1',
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET || 'production',
   useCdn: true,
   apiVersion: '2024-01-01',
 })
