77ef77ef0a5cd49d04c684d2c9bc66d0d88223ed
Ollie <info@ollieadegboye.com>
2026-06-05 23:44:03 +0100

    Disable CDN cache for fresh content

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || 'rscqwpq1',
   dataset: import.meta.env.PUBLIC_SANITY_DATASET || 'production',
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2024-03-01',
 })
