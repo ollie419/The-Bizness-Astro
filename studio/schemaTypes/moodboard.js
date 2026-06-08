b4f53737dc504927a88d3a3d04aa0869d5fcf181
Ollie <info@ollieadegboye.com>
2026-06-08 11:23:19 +0100

    IMG archive dropdown

--- studio/schemaTypes/moodboard.js
+++ studio/schemaTypes/moodboard.js
@@ -2,28 +2,43 @@ export default {
   name: 'moodboard',
   title: 'IMG',
   type: 'document',
+  orderings: [
+    {
+      title: 'Date, Newest First',
+      name: 'dateDesc',
+      by: [{ field: 'date', direction: 'desc' }]
+    }
+  ],
   preview: {
-    prepare() {
-      return { title: 'IMG Page' }
+    select: { title: 'title', date: 'date' },
+    prepare({ title, date }) {
+      return { title: title || date || 'IMG Entry' }
     }
   },
   fields: [
     {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'e.g. June 2025',
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'date',
+      options: { dateFormat: 'MMMM YYYY' },
+      description: 'Month and year for archive ordering',
+    },
+    {
       name: 'curator',
       title: 'Curated by',
       type: 'string',
-      description: 'Name of the curator e.g. "Ollie"',
     },
     {
       name: 'images',
       title: 'Images',
       type: 'array',
-      of: [
-        {
-          type: 'image',
-          options: { hotspot: true },
-        }
-      ],
+      of: [{ type: 'image', options: { hotspot: true } }],
     },
   ],
 }
